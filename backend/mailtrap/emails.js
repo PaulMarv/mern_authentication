668eb05effa2359be3225b50d4cfb64003f2ca94
PaulMarv <mpsalako@gmail.com>
2025-03-15 19:56:20 +0100

    welcome email sent

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,19 +1,44 @@
 import { mailtrapClient, sender } from "../config/mailtrap.config.js";
 import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
 
+
+const MAILTRAP_WELCOME_TEMPLATE_UUID = process.env.MAILTRAP_WELCOME_TEMPLATE_UUID;
+
 export const sendVerificationEmail = async (email, verificationToken) => {
-    const recepient = [{ email }];
-    try {
-        const response = await mailtrapClient.send({
-            from: sender,
-            to: recepient,
-            subject: "Verify your email",
-            html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken ),
-            category: "Verification Email",
-        })
-        console.log("Verification email sent successfully", response);
-    } catch (error) {
-        console.error("Error sending verification email", error);
-        throw new Error(`Error sending verification email: ${error.message}`);
-    }
+  const recepient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recepient,
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Verification Email",
+    });
+    console.log("Verification email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending verification email", error);
+    throw new Error(`Error sending verification email: ${error.message}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: MAILTRAP_WELCOME_TEMPLATE_UUID,
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name,
+      },
+    });
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error("Error sending welcome email", error);
+    throw new Error(`Error sending welcome email: ${error.message}`);
+  }
 };
